c8ae6416cc632e6a27a3ef41503a5a2b439a040b
lawrencecrowther <loz@snyk.io>
2025-10-25 14:49:26 +1000

    Initial Commit

---
+++ BookReview/BookReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookReviewApp.swift
+//  BookReview
+//
+//  Created by Lawrence Crowther on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookReview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BookReview
+//
+//  Created by Lawrence Crowther on 25/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
